81f014d7f92ccc32a7e8a136c3aad491e51ff17d
Chris Angelico <rosuav@gmail.com>
2026-09-11 22:18:15 +1000

    Tinker with the tag syntax, definite WIP

--- lib/markdown_form.rb
+++ lib/markdown_form.rb
@@ -7,12 +7,25 @@ module Jekyll
 
     def initialize(tag_name, text, tokens)
       super
-      @text = text
-      puts "Hello, world!"
+      text.strip!
+      if m = text.match(/^([A-Za-z0-9]+)(\??): (.*)$/)
+        @name = m[1]
+        @type = m[2] == "?" ? "checkbox" : "text"
+        @label = m[3]
+      else
+        @name = text.downcase
+        @type = "text"
+        @label = text
+      end
+      if @name == "submit" then @type = "submit" end
     end
 
     def render(context)
-      "<input> #{@text}"
+      case @type
+      when "checkbox" then "<label><input type=checkbox name=#{@name}> #{@label}"
+      when "submit" then "<button type=submit>#{@label}</button>"
+      else "<label>#{@label}: <input name=#{@name}></label>"
+      end
     end
   end
 end
